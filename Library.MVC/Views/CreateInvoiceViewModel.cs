c9da74cdef2a85328de9ede1158384d268ff7368
Winou <84401927+Winou-1@users.noreply.github.com>
2026-03-10 18:28:49 +0100

    finito

---
+++ Library.MVC/Views/CreateInvoiceViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.MVC.ViewModels
+{
+    public class CreateInvoiceViewModel
+    {
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public DateTime InvoiceDate { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        public List<SelectListItem> Customers { get; set; } = new();
+        public List<SelectListItem> Products { get; set; } = new();
+    }
+}
